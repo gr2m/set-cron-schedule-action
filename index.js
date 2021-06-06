8b08a52ebc08831848fc1ef649c7e75b1925c8fd
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-06-06 15:36:21 -0700

    feat: initial version

---
+++ index.js
@@ -0,0 +1,77 @@
+const { Octokit } = require("octokit");
+const core = require("@actions/core");
+const yaml = require("js-yaml");
+
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone"); // dependent on utc plugin
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+module.exports = main().catch((error) => {
+  console.log(error);
+  core.setFailed(error.message);
+});
+
+async function main() {
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+  const cronExpressions = core.getInput("crons").split("\n");
+  const message =
+    core.getInput("message") ||
+    "ci($WORKFLOW_NAME): update cron expression to `$CRON_EXPRESSION`";
+
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+    userAgent: "iterate-cron-action",
+    retry: { enabled: false },
+  });
+
+  // get current workflow path by name
+  const workflows = await octokit.paginate(
+    "GET /repos/{owner}/{repo}/actions/workflows",
+    {
+      owner,
+      repo,
+    }
+  );
+  const currentWorkflow = workflows.find(
+    (workflow) => workflow.name === process.env.GITHUB_WORKFLOW
+  );
+
+  // get current workflow
+  const { data } = await octokit.request(
+    "GET /repos/{owner}/{repo}/contents/{path}",
+    {
+      owner,
+      repo,
+      path: currentWorkflow.path,
+    }
+  );
+  const workflow = yaml.load(Buffer.from(data.content, "base64").toString());
+
+  // update cron expression in workflow
+  const currentExpression = workflow.on.schedule[0].cron;
+  const currentIndex = cronExpressions.indexOf(currentExpression);
+  const nextExpression =
+    cronExpressions[(currentIndex + 1) % cronExpressions.length];
+
+  workflow.on.schedule[0].cron = nextExpression;
+
+  const newContent = yaml.dump(workflow, {
+    quotingType: '"'
+  });
+
+  // update workflow file
+  await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+    owner,
+    repo,
+    path: currentWorkflow.path,
+    content: Buffer.from(newContent).toString("base64"),
+    message: message
+      .replace("$WORKFLOW_NAME", process.env.GITHUB_WORKFLOW)
+      .replace("$CRON_EXPRESSION", nextExpression),
+    sha: data.sha
+  });
+
+  console.log("workflowfile updated");
+}
